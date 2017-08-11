d4a94b4c063c1af2af03e037fcb702705094c722
Kevin Lin <kevin@quilt.io>
2017-08-11 10:06:52 -0700

    blueprint: Lint all files

--- slackin.js
+++ slackin.js
@@ -1,20 +1,16 @@
-"use strict";
+const { Container, Service, publicInternet } = require('@quilt/quilt');
 
-var { Container, Service, publicInternet } = require("@quilt/quilt");
+exports.New = function New(slackTeamId, slackToken, port = 80) {
+  const container = new Container('quilt/slackin', [
+    'slackin', '--port', port.toString(),
+  ]).withEnv({
+    SLACK_SUBDOMAIN: slackTeamId,
+    SLACK_API_TOKEN: slackToken,
+  });
 
-exports.New = function(slack_team_id, slack_token, port = 80) {
-	var container = new Container(
-			"quilt/slackin",
-			["slackin", "--port", port.toString()]
-		).withEnv({
-			SLACK_SUBDOMAIN: slack_team_id,
-			SLACK_API_TOKEN: slack_token
-		}
-	);
+  const slackin = new Service('slackin', [container]);
+  slackin.allowFrom(publicInternet, port);
+  publicInternet.allowFrom(slackin, 443);
 
-	var slackin = new Service("slackin", [container]);
-	slackin.allowFrom(publicInternet, port);
-	publicInternet.allowFrom(slackin, 443);
-
-	return slackin;
+  return slackin;
 };
